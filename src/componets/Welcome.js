1cbc6b2da9df77a34888ebfcc0c0a9febdc2b0df
9182029380 <102147569+9182029380@users.noreply.github.com>
2024-01-29 17:57:14 +0530

    Update Welcome.js

--- src/componets/Welcome.js
+++ src/componets/Welcome.js
@@ -2,7 +2,7 @@ import React from 'react'
 
 function  Welcome()  {
   return (
-    <div>hi everyone welcome to sharath repo </div>
+    <div>hi everyone welcome to sharath repo and Sohel rep </div>
   )
 }
 export default Welcome;
